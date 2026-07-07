b49125ef890c67168ba29600eb5bbd0345094341
lailasabila955-tech <lailasabila955@gmail.com>
2026-07-07 09:45:31 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,14 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
 
+  eleventyConfig.addFilter("date", function (dateObj, format) {
+    const d = new Date(dateObj);
+    if (format === "MMMM yyyy") {
+      return d.toLocaleString("en-US", { month: "long", year: "numeric" });
+    }
+    return d.toISOString();
+  });
+
   eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md").sort((a, b) => b.date - a.date);
   });
